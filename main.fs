ecf5bdfb68769d0959a1164e5d0dfa689289a52b
wa1tnr <wa1tnr@users.noreply.github.com>
2022-01-06 16:24:55 +0000

    nice test messages incl blank

--- main.fs
+++ main.fs
@@ -47,6 +47,9 @@ turnkey
 ;
 
 : demf \ no stack effects
+;
+
+: demnoneaa
 
     127 #,  lv0!
       0 #,  lv1!
@@ -79,6 +82,21 @@ turnkey
       6 #,  cmd!
     timex
       1 #,  cmd!
+;
+
+: blank
+  gl_ lv3!
+  gl_ lv2!
+  gl_ lv1!
+  gl_ lv0!
+;
+
+: demg \ no stack effects
+  gl7 lv3!
+  gl6 lv2!
+  gl5 lv1!
+  gl4 lv0!
+  6 #, cmd!
 ;
 
 : noppp 1 #, drop ;
