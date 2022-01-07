acce5646adc4002fc910b013b3f38f60049d9c67
wa1tnr <wa1tnr@users.noreply.github.com>
2022-01-07 23:32:10 +0000

    interstices

--- rp2040-sh_regForth-bb/app.fs
+++ rp2040-sh_regForth-bb/app.fs
@@ -11,64 +11,175 @@
 
   ." STACK: " .s cr
 
+  gl_ lv2! stimex \ blank 2 write 3
   gl_ lv3! stimex
+
+  gl_ lv1! stimex \ blank 1 write 2
   gl_ lv2! stimex
+
+  gl_ lv0! stimex \ blank 0 write 1
   gl_ lv1! stimex
+
+  \ write zero for the stanza - no blank needed
+
   gl_ lv0! stimex stimex stimex stimex stimex stimex
 
+
+
+
+  gl_ lv2! stimex \ blank 2 write 3
   gl_ lv3! stimex
+
+  gl_ lv1! stimex \ blank 1 write 2
   gl_ lv2! stimex
+
+  gl_ lv0! stimex \ blank 0 write 1
   gl_ lv1! stimex
+
+  \ write zero for the stanza - no blank needed
+
   gl8 lv0! stimex stimex stimex stimex stimex stimex
 
+
+
+
+  gl_ lv2! stimex \ blank 2 write 3
   gl_ lv3! stimex
+
+  gl_ lv1! stimex \ blank 1 write 2
   gl_ lv2! stimex
+
+  gl_ lv0! stimex \ blank 0 write 1
   gl8 lv1! stimex
+
+  \ write zero for the stanza - no blank needed
+
   gl6 lv0! stimex stimex stimex stimex stimex stimex
 
+
+
+
+  gl_ lv2! stimex \ blank 2 write 3
   gl_ lv3! stimex
+
+  gl_ lv1! stimex \ blank 1 write 2
   gl8 lv2! stimex
+
+  gl_ lv0! stimex \ blank 0 write 1
   gl6 lv1! stimex
+
+  \ write zero for the stanza - no blank needed
+
   gl7 lv0! stimex stimex stimex stimex stimex stimex
 
+
+
+  gl_ lv2! stimex \ blank 2 write 3
   gl8 lv3! stimex
+
+  gl_ lv1! stimex \ blank 1 write 2
   gl6 lv2! stimex
+
+  gl_ lv0! stimex \ blank 0 write 1
   gl7 lv1! stimex
+
+  \ write zero for the stanza - no blank needed
+
   gl5 lv0! stimex stimex stimex stimex stimex stimex
 
+
+  gl_ lv2! stimex \ blank 2 write 3
   gl6 lv3! stimex
+
+  gl_ lv1! stimex \ blank 1 write 2
   gl7 lv2! stimex
+
+  gl_ lv0! stimex \ blank 0 write 1
   gl5 lv1! stimex
+
+  \ write zero for the stanza - no blank needed
+
   gl3 lv0! stimex stimex stimex stimex stimex stimex
 
+
+  gl_ lv2! stimex \ blank 2 write 3
   gl7 lv3! stimex
+
+  gl_ lv1! stimex \ blank 1 write 2
   gl5 lv2! stimex
+
+  gl_ lv0! stimex \ blank 0 write 1
   gl3 lv1! stimex
+
+  \ write zero for the stanza - no blank needed
+
   gl0 lv0! stimex stimex stimex stimex stimex stimex
 
+
+  gl_ lv2! stimex \ blank 2 write 3
   gl5 lv3! stimex
+
+  gl_ lv1! stimex \ blank 1 write 2
   gl3 lv2! stimex
+
+  gl_ lv0! stimex \ blank 0 write 1
   gl0 lv1! stimex
+
+  \ write zero for the stanza - no blank needed
+
   gl9 lv0! stimex stimex stimex stimex stimex stimex
 
+  gl_ lv2! stimex \ blank 2 write 3
   gl3 lv3! stimex
+
+  gl_ lv1! stimex \ blank 1 write 2
   gl0 lv2! stimex
+
+  gl_ lv0! stimex \ blank 0 write 1
   gl9 lv1! stimex
+
+  \ write zero for the stanza - no blank needed
+
   gl_ lv0! stimex stimex stimex stimex stimex stimex
 
+  gl_ lv2! stimex \ blank 2 write 3
   gl0 lv3! stimex
+
+  gl_ lv1! stimex \ blank 1 write 2
   gl9 lv2! stimex
+
+  gl_ lv0! stimex \ blank 0 write 1
   gl_ lv1! stimex
+
+  \ write zero for the stanza - no blank needed
+
   gl_ lv0! stimex stimex stimex stimex stimex stimex
 
+
+  gl_ lv2! stimex \ blank 2 write 3
   gl9 lv3! stimex
+
+  gl_ lv1! stimex \ blank 1 write 2
   gl_ lv2! stimex
+
+  gl_ lv0! stimex \ blank 0 write 1
   gl_ lv1! stimex
+
+  \ write zero for the stanza - no blank needed
+
   gl_ lv0! stimex stimex stimex stimex stimex stimex
 
+  gl_ lv2! stimex \ blank 2 write 3
   gl_ lv3! stimex
+
+  gl_ lv1! stimex \ blank 1 write 2
   gl_ lv2! stimex
+
+  gl_ lv0! stimex \ blank 0 write 1
   gl_ lv1! stimex
+
+  \ write zero for the stanza - no blank needed
+
   gl_ lv0! stimex stimex stimex stimex stimex stimex
 
   again
